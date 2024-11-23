570e5b9f18f87e5438884ac3b850835f092077cf
oferitz <ofer.webdev@gmail.com>
2024-11-23 15:45:35 +0200

    nicer look

--- src/app/components/UserUrls.tsx
+++ src/app/components/UserUrls.tsx
@@ -6,7 +6,7 @@ interface UserUrlsProps {
 }
 export default function UserUrls({ urls, onRemove }: UserUrlsProps) {
   return urls.length > 0 ? (
-    <div className="mt-4 flex gap-2 flex-wrap">
+    <div className="mt-2 w-full mx-auto max-w-3xl flex gap-2 flex-wrap">
       {getValidImdbIds(urls).map((id) => (
         <Chip key={id} onClose={() => onRemove(id)} className="cursor-pointer">
           {id}
